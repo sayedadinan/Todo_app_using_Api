3ba0fd9d9b290bae5cdfaad3756eea27afaf3ba5
sayed adinan <147618795+sayedadinan@users.noreply.github.com>
2024-03-02 10:55:02 +0530

    todo almost completed now working on refactoring

---
+++ lib/view/todo_list.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:todo_api/services/todo_service.dart';
+import 'package:todo_api/view/work_add.dart';
+import 'package:todo_api/widgets/todo_card.dart';
+
+class TodoListpage extends StatefulWidget {
+  const TodoListpage({super.key});
+
+  @override
+  State<TodoListpage> createState() => _TodoListpageState();
+}
+
+class _TodoListpageState extends State<TodoListpage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchtodo();
+  }
+
+  bool isloading = true;
+  List items = [];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'todo list',
+        ),
+      ),
+      body: Visibility(
+        visible: isloading,
+        child: Center(
+          child: CircularProgressIndicator(),
+        ),
+        replacement: RefreshIndicator(
+          onRefresh: fetchtodo,
+          child: Visibility(
+            visible: items.isNotEmpty,
+            replacement: const Center(
+              child: Text('THERE IS NOT DATA'),
+            ),
+            child: ListView.builder(
+                itemCount: items.length,
+                padding: const EdgeInsets.all(8),
+                itemBuilder: (context, index) {
+                  final item = items[index] as Map;
+                  final id = item['_id'] as String;
+                  return Todocard(
+                    index: index,
+                    item: item,
+                    deletbyid: deletebyid,
+                    navigateEdit: navigatetoeditpage,
+                  );
+                }),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            navigatetoanotherpage();
+          },
+          label: const Text('Add Todo')),
+    );
+  }
+
+  void navigatetoanotherpage() {
+    final route = MaterialPageRoute(builder: (context) => const Addtodo());
+    Navigator.push(context, route);
+  }
+
+  void navigatetoeditpage(Map item) async {
+    final route = MaterialPageRoute(builder: (context) => Addtodo(todo: item));
+    await Navigator.push(context, route);
+    setState(() {
+      isloading = true;
+    });
+    fetchtodo();
+  }
+
+  deletebyid(String id) async {
+    final isSucces = await TodoService.deletedbyid(id);
+    if (isSucces) {
+      final filters = items.where((element) => element['_id'] != id).toList();
+      setState(() {
+        items = filters;
+      });
+    } else {
+      print('deletion failed');
+    }
+  }
+
+  Future<void> fetchtodo() async {
+    final response = await TodoService.fetchdata();
+    if (response != null) {
+      setState(() {
+        items = response;
+      });
+    }
+    setState(() {
+      isloading = false;
+    });
+  }
+}
---
+++ lib/view/work_add.dart
@@ -0,0 +1,157 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:todo_api/view/todo_list.dart';
+
+class Addtodo extends StatefulWidget {
+  final Map? todo;
+  const Addtodo({super.key, this.todo});
+
+  @override
+  State<Addtodo> createState() => _AddtodoState();
+}
+
+class _AddtodoState extends State<Addtodo> {
+  TextEditingController titlecontroller = TextEditingController();
+  TextEditingController descriptioncontroller = TextEditingController();
+  bool isEdit = false;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    final todo = widget.todo;
+    if (todo != null) {
+      isEdit = true;
+      final title = todo['title'];
+      final describtion = todo['description'];
+      titlecontroller.text = title;
+      descriptioncontroller.text = describtion;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(isEdit ? 'Edit Todo' : 'Add Todo'),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(20),
+        children: [
+          TextFormField(
+            controller: titlecontroller,
+            decoration:
+                const InputDecoration(hintText: 'title add for the work'),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          TextField(
+            controller: descriptioncontroller,
+            decoration: const InputDecoration(hintText: 'Description'),
+            keyboardType: TextInputType.multiline,
+            minLines: 5,
+            maxLines: 8,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              isEdit ? editedData() : submitData();
+              navigatetohomepage(context);
+            },
+            child: Text(isEdit ? 'update' : 'Submit'),
+          )
+        ],
+      ),
+    );
+  }
+
+  editedData() async {
+    final todo = widget.todo;
+    if (todo == null) {
+      print('yoiu can not call updated without todo data');
+      return;
+    }
+    final id = todo['_id'];
+    final title = titlecontroller.text;
+    final description = descriptioncontroller.text;
+    final body = {
+      "title": title,
+      "description": description,
+      "is_completed": false,
+    };
+    final url = 'https://api.nstack.in/v1/todos/$id';
+    final uri = Uri.parse(url);
+    final response = await http.put(
+      uri,
+      body: jsonEncode(body),
+      headers: {'Content-Type': 'application/json'},
+    );
+    if (response.statusCode == 200) {
+      print('Updation success ');
+      showSuccessMessage('Updation Success');
+    } else {
+      print('error');
+      showerrorMessage('updation Failed');
+    }
+  }
+
+  submitData() async {
+    final title = titlecontroller.text;
+    final description = descriptioncontroller.text;
+    final body = {
+      "title": title,
+      "description": description,
+      "is_completed": false,
+    };
+    final url = 'https://api.nstack.in/v1/todos';
+    final uri = Uri.parse(url);
+    final response = await http.post(
+      uri,
+      body: jsonEncode(body),
+      headers: {'Content-Type': 'application/json'},
+    );
+    if (response.statusCode == 201) {
+      titlecontroller.text = '';
+      descriptioncontroller.text = '';
+      print('success');
+      showSuccessMessage('Creation Success');
+    } else {
+      print('error');
+      showerrorMessage('its not adding successfully');
+    }
+    print(response.body);
+  }
+
+  void navigatetohomepage(BuildContext context) async {
+    final route = MaterialPageRoute(builder: (context) => const TodoListpage());
+    await Navigator.pushAndRemoveUntil(
+      context,
+      route,
+      (route) =>
+          false, // This predicate function ensures that all routes are removed
+    );
+  }
+
+  showSuccessMessage(String message) {
+    final snackBar = SnackBar(
+        content: Text(
+      message,
+      style: const TextStyle(color: Colors.green),
+    ));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  showerrorMessage(String message) {
+    final snackBar = SnackBar(
+        backgroundColor: Colors.red,
+        content: Text(
+          message,
+          style: const TextStyle(color: Colors.white),
+        ));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+}
